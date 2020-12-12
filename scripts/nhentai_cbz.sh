21e22f171f93718bc6fd51abe9e6af0757c0304b
hrlou <hess@hesslewis.com>
2020-12-12 15:08:37 -0500

    better db script

--- scripts/nhentai_cbz.sh
+++ scripts/nhentai_cbz.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 find . -name '*.txt' -print0 | while IFS= read -r -d '' line; do 
-    # echo "$line"
-    id=${line##*/}
-    id=${id//.txt}
+    id=${line##*/}; id=${id//.txt}
     count=$(echo "$line" | sed "s/$id/$id\n/g" | grep -c "$id")
     if [ "$count" -ge 2 ]; then
         zip -r "$(echo "$line" | sed "s/\/$id.txt/.cbz/")" "$(echo "$line" | sed "s/\/$id.txt//")" && rm -r "$(echo "$line" | sed "s/\/$id.txt//")"
